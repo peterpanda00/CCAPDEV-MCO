358aeb6a1cab7b7453417592a35cfae08afacaf8
Johanna Aperin <johannaaperin@gmail.com>
2023-08-03 21:59:55 +0800

    Reviews with style of images

--- frontend/src/components/ReviewsDetails.js
+++ frontend/src/components/ReviewsDetails.js
@@ -1,24 +1,38 @@
 const ReviewDetails = ({ review }) => {
-    return (
-      <div id="post-container">
-        <h4>
-          <strong>{review.userName}</strong>
-        </h4>
-        <p>{review.revContent}</p>
+  return (
+    <div id="post-container">
+      <h4>
+        <strong>{review.userName}</strong>
+      </h4>
+      <p>{review.revContent}</p>
 
-        {review.reviewImg && (
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
+      {review.reviewImg && (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '300px', 
+            border: '1px solid #ddd',
+            padding: '10px',
+          }}
+        >
           <img
             src={review.reviewImg}
             alt="Review Image"
-            style={{ maxWidth: '300px', maxHeight: '300px' }}
+            style={{
+              maxWidth: '100%',
+              maxHeight: '100%', 
+              width: 'auto',
+              height: 'auto',
+            }}
           />
         </div>
       )}
-      
-        <p>{review.createdAt}</p>
-      </div>
-    );
-  };
-  
-  export default ReviewDetails;
+
+      <p>{review.createdAt}</p>
+    </div>
+  );
+};
+
+export default ReviewDetails;
